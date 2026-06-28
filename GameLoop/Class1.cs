741d42758896c95cf126131186970127f14416b4
kikiki <k.maski1212@gmail,com>
2026-06-29 04:05:17 +0900

    8.9章

---
+++ GameLoop/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+
+namespace GameLoop {
+    public class Class1 {
+        while(true){
+            processInput();
+        Update();
+        render();
+
+    }
+
+    public class Game {
+        private const double MS_PER_UPDATE = 16.6667;
+
+        public void Run() {
+            double previous = GetCurrentTime();
+            double lag = 0.0;
+
+            while (true) {
+                double current = GetCurrentTime();
+                double elapsed = current - previous;
+                previous = current;
+
+                lag += elapsed;
+
+                ProcessInput();
+
+                while (lag >= MS_PER_UPDATE) {
+                    Update();
+                    lag -= MS_PER_UPDATE;
+                }
+
+                Render();
+            }
+        }
+
+        private double GetCurrentTime() {
+            return Environment.TickCount64;
+        }
+
+        private void ProcessInput() {
+        }
+
+        private void Update() {
+        }
+
+        private void Render() {
+        }
+    }
+}
---
+++ UpdateMethod/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace UpdateMethod {
+    public class Class1 {
+        public class Statue {
+            private int frames_;
+
+            public Statue() {
+                frames_ = 0;
+            }
+
+            public void Update() {
+                if (++frames_ == 30) {
+                    ShootLightning();
+
+                    frames_ = 0;
+                }
+            }
+
+            private void ShootLightning() {
+                Console.WriteLine("雷を撃つ");
+            }
+        }
+
+        public abstract class Entity {
+            public abstract void Update();
+        }
+        public class Statue : Entity {
+            private int frames_;
+
+            public Statue() {
+                frames_ = 0;
+            }
+
+            public override void Update() {
+                frames_++;
+
+                if (frames_ == 30) {
+                    ShootLightning();
+                    frames_ = 0;
+                }
+            }
+
+            private void ShootLightning() {
+                Console.WriteLine("女神像が雷を放った！");
+            }
+        }
+
+        Entity statue = new Statue();
+
+    }
+}
